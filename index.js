e4f669eef9484790e33b4a5693a8e4e1faa94973
sunli <sunli@cdv.com>
2016-09-29 11:23:30 +0800

    Signed-off-by: sunli <sunli@cdv.com>

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-(function(){
-	//私有静态成员
-	var user = "";
-	
-	//私有静态方法
-	function privateStaticMethod(){
-	}
+function myConstructor(message) {
+    this.myMsaage = message;
 
-	
-	Box = function(value){
-		//私有成员
-		var privateStaticUser = value; 
-		
-		//这个是私有方法
-		function privateMethod(){
-		}
+    // 私有属性
+    var separate = ' -';
+    var myOwner = this;
 
-		
-		//公有方法，因为能访问私有成员，也可以说是特权函数，也可以说是实例方法
-		this.getUser = function(){
-			return user;
-		};		
-		
-		//公有成员
-		this.user = 1;
-	};
-	
-	//公有共享访问
-	Box.prototype.sharedMethod = function () {};
-	
-	//公有共享属性
-	Box.prototype.sharedProperty = function () {};
+    // 私有方法
+    function alertMessage() {
+        alert(myOwner.myMsaage);
+    }
 
-	
-	//公有静态方法 
-	Box.staticMethod = function(){};
-	
-	//公有静态成员
-	Box.staticProperty = 1; 
-})();
+    alertMessage();
+
+    // 特权方法(也是公有方法)
+    this.appendToMessage = function(string) {
+        this.myMsaage += separate + string;　　　　
+        alertMessage();
+    };
+
+    // 公有方法
+    myConstructor.prototype.clearMessage = function() {
+        this.myMsaage = '';
+    }
+    // 静态属性  
+    myConstructor.name = 'Jeff';
+    
+    // 静态方法
+    myConstructor.alertName = function() {
+        alert(this.name);
+    }
+}
